82de8811359d4f69d9e8d03b8bfd1579cf508245
ImerAl <40179626+ImerAl@users.noreply.github.com>
2020-04-24 15:39:55 -0600

    Request controler almost done

--- ProyectWeb/src/sv/edu/udb/controllers/RequestController.java
+++ ProyectWeb/src/sv/edu/udb/controllers/RequestController.java
@@ -6,45 +6,74 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import sv.edu.udb.models.Request;
+import sv.edu.udb.models.RequestDAO;
 
 /**
  *
  * @author Imer Palma
  */
-@WebServlet(urlPatterns = {"/RequestController"})
+@WebServlet(name = "RequestController", urlPatterns = {"/request.do"})
 public class RequestController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    ArrayList<String> listaErrores = new ArrayList<>();
+    Request modelRequest = new Request();
+    RequestDAO rqDAO = new RequestDAO();
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet RequestController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet RequestController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        throws ServletException, IOException{
+    
+        response.setContentType("text/html;charset=UTF -8");
+        try (PrintWriter out = response.getWriter()){
+            if (request.getParameter("op")== null) {
+               list(request, response);
+               return;
+            }
+            String operacion = request.getParameter("op");
+            switch(operacion){
+                case "list":
+                    list(request,response);
+                    break;
+                case "new":
+                    request.getRequestDispatcher("").forward(request, response);
+                    break;
+                case "insert":
+                    insert(request,response);
+                    break;
+                case "get":
+                     getRequest(request, response);
+                     break;
+                case "update":
+                    update(request,response);
+                    break;
+                case "delete":
+                    delete(request, response);
+                    break;
+            }
+        } catch (Exception e) {
         }
+        
     }
-
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -83,5 +112,64 @@ public class RequestController extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+//    @Override
+//    public String getServletInfo() {
+//        return "Short description";
+//    }// </editor-fold>
+    private void list(HttpServletRequest request, HttpServletResponse response){
+        try {
+            request.setAttribute("listRequest", rqDAO.getAll());
+            request.getRequestDispatcher("").forward(request, response);
+        } catch (ServletException | IOException ex) {
+            Logger.getLogger(RequestController.class.getName()).log(Level.SEVERE,null,ex);
+        }
+    }
+    private void insert(HttpServletRequest request, HttpServletResponse response){
+        try {
+            listaErrores.clear();
+            modelRequest.setIdTypeRequest(0);
+            modelRequest.setDepartmentId(0);
+            modelRequest.setProjectId(0);
+            modelRequest.setRequestDate(requestDate);
+            modelRequest.setRequestStatus(requestStatus);
+            modelRequest.setRequestDescription(requestDescription);
+            request.setAttribute("request", rqDAO.save(modelRequest) );
+            request.getRequestDispatcher("").forward(request, response);
+        } catch (IOException | ServletException ex) {
+            Logger.getLogger(RequestController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    private void getRequest(HttpServletRequest request, HttpServletResponse response){
+        try{
+            String idrq = request.getParameter("id");
+            int id = Integer.parseInt(idrq);
+            if (rqDAO.getOneById(id) != null) {
+                request.setAttribute("request", rqDAO.getOneById(id));
+                request.getRequestDispatcher("").forward(request, response);
+            }else{
+                response.sendRedirect("");
+            }
+        }catch(IOException | ServletException ex){
+            Logger.getLogger(RequestController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    private void update(HttpServletRequest request, HttpServletResponse response){
+        try {
+            listaErrores.clear();
+            modelRequest.setIdTypeRequest(0);
+            modelRequest.setDepartmentId(0);
+            modelRequest.setProjectId(0);
+            modelRequest.setRequestDate(requestDate);
+            modelRequest.setRequestStatus(requestStatus);
+            modelRequest.setRequestDescription(requestDescription);
+            request.setAttribute("request", rqDAO.updateStatus(modelRequest) );
+            request.getRequestDispatcher("").forward(request, response);
+        } catch (IOException | ServletException ex) {
+            Logger.getLogger(RequestController.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
+    }
+    private void delete(HttpServletRequest request, HttpServletResponse response){
+        
+    }
 }

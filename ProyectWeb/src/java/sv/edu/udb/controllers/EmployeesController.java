f0142b37466074a603dbfef5a20727f9bd658cbf
IvonneSoriano <ivonnesoriano25@gmail.com>
2020-05-03 14:24:28 -0600

    view and delete are working

--- ProyectWeb/src/java/sv/edu/udb/controllers/EmployeesController.java
+++ ProyectWeb/src/java/sv/edu/udb/controllers/EmployeesController.java
@@ -3,30 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package sv.edu.udb.controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.sql.SQLException;
 import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.apache.log4j.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import sv.edu.udb.models.Employee;
-import sv.edu.udb.models.EmployeeDAO;
 import sv.edu.udb.models.Deparment;
 import sv.edu.udb.models.DeparmentDAO;
+import sv.edu.udb.models.Employee;
+import sv.edu.udb.models.EmployeeDAO;
 import sv.edu.udb.models.Rol;
 import sv.edu.udb.models.RolDAO;
+
 /**
  *
- * @author Imer Palma
+ * @author kiss_
  */
-@WebServlet(name = "EmployeeController", urlPatterns = {"/empleado.do"})
-public class EmployeeController extends HttpServlet {
+@WebServlet(name = "EmployeesController", urlPatterns = {"/empleados.do"})
+public class EmployeesController extends HttpServlet {
+    private static Logger logger = Logger.getLogger(ProjectController.class);
 
     ArrayList<String> listaErrores = new ArrayList<>();
     Employee modelEmployee = new Employee();
@@ -37,21 +39,29 @@ public class EmployeeController extends HttpServlet {
     RolDAO rolDAO = new RolDAO();
     
     
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException{
-    
-        response.setContentType("text/html;charset=UTF -8");
-        try (PrintWriter out = response.getWriter()){
-            if (request.getParameter("op")== null) {
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+           if (request.getParameter("op")== null) {
                list(request, response);
                return;
             }
             String operacion = request.getParameter("op");
             switch(operacion){
-                case "list":
+                case "ver":
                     list(request,response);
                     break;
-                case "new":
+                case "crear":
                     newEmployee(request, response);
                     break;
                 case "insert":
@@ -67,21 +77,12 @@ public class EmployeeController extends HttpServlet {
                     delete(request, response);
                     break;
             }
-        } catch (Exception e) {
+        }
+            catch (Exception e) {
         }
         
     }
-    
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    }
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    }
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -91,7 +92,12 @@ public class EmployeeController extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -100,7 +106,11 @@ public class EmployeeController extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
     /**
      * Returns a short description of the servlet.
@@ -111,16 +121,16 @@ public class EmployeeController extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-//    @Override
-//    public String getServletInfo() {
-//        return "Short description";
-//    }// </editor-fold>
-    private void list(HttpServletRequest request, HttpServletResponse response){
+
+    
+    
+    
+     private void list(HttpServletRequest request, HttpServletResponse response){
         try {
             request.setAttribute("listEmployees", empDAO.getAll());
             request.getRequestDispatcher("/employeeView/ListEmployee.jsp").forward(request, response);
-        } catch (ServletException | IOException ex) {
-            Logger.getLogger(EmployeeController.class.getName()).log(Level.SEVERE,null,ex);
+        } catch (ServletException | IOException e) {
+            logger.error("Error in List method. Message: " + e.getMessage());
         }
     }
     private void insert(HttpServletRequest request, HttpServletResponse response){
@@ -139,16 +149,16 @@ public class EmployeeController extends HttpServlet {
             }else{
                 if(empDAO.save(modelEmployee)){
                     request.getSession().setAttribute("exito", "Empleado registrado exitosamente");
-                    response.sendRedirect(request.getContextPath() + "/employee.do?op=list");
+                    response.sendRedirect(request.getContextPath() + "/empleados.do?op=ver");
                 }
                 else{
                     request.getSession().setAttribute("fracaso", "Empleado no ha sido ingresado");
-                    response.sendRedirect(request.getContextPath() + "/employee.do?op=list");
+                    response.sendRedirect(request.getContextPath() + "/empleados.do?op=ver");
                 }
             }
             
-        }catch(IOException | ServletException ex){
-             Logger.getLogger(EmployeeController.class.getName()).log(Level.SEVERE, null, ex);
+        }catch(IOException | ServletException e){
+             logger.error("Error in insert Employees method. Message: " + e.getMessage());
         }
     }
     private void getEmployee(HttpServletRequest request, HttpServletResponse response){
@@ -159,12 +169,12 @@ public class EmployeeController extends HttpServlet {
                 request.setAttribute("employee", empDAO.getEmployeeById(id));
                 request.setAttribute("listRol", rolDAO.getAll());
                 request.setAttribute("listDeparment", depDao.getAll());               
-                request.getRequestDispatcher("/EmployeeView/EditEmployee.jsp").forward(request, response);
+                request.getRequestDispatcher("/employeeView/EditEmployee.jsp").forward(request, response);
             }else{
                 response.sendRedirect("");
             }
-        }catch(IOException | ServletException ex){
-            Logger.getLogger(EmployeeController.class.getName()).log(Level.SEVERE, null, ex);
+        }catch(IOException | ServletException e){
+            logger.error("Error in getEmployees method. Message: " + e.getMessage());
         }
     }
     private void update(HttpServletRequest request, HttpServletResponse response){
@@ -183,16 +193,16 @@ public class EmployeeController extends HttpServlet {
             }else{
                 if(empDAO.updateUser(modelEmployee, request.getParameter("newPasword"))){
                     request.getSession().setAttribute("exito", "Empleado modificado");
-                    response.sendRedirect(request.getContextPath() + "/employee.do?op=list");
+                    response.sendRedirect(request.getContextPath() + "/empleados.do?op=ver");
                 }
                 else{
                     request.getSession().setAttribute("fracaso", "Empleado no ha sido modificado");
-                    response.sendRedirect(request.getContextPath() + "/employee.do?op=list");
+                    response.sendRedirect(request.getContextPath() + "/empleados.do?op=ver");
                 }
             }
        
-        } catch (IOException | ServletException ex) {
-            Logger.getLogger(EmployeeController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException | ServletException e) {
+            logger.error("Error in updateEmployees method. Message: " + e.getMessage());
         }
 
     }
@@ -201,31 +211,30 @@ public class EmployeeController extends HttpServlet {
             
                 request.setAttribute("listRol", rolDAO.getAll());
                 request.setAttribute("listDeparment", depDao.getAll());               
-                request.getRequestDispatcher("/EmployeeView/NewEmployee.jsp").forward(request, response);
+                request.getRequestDispatcher("/employeeView/NewEmployee.jsp").forward(request, response);
             
-        }catch(IOException | ServletException ex){
-            Logger.getLogger(EmployeeController.class.getName()).log(Level.SEVERE, null, ex);
+        }catch(IOException | ServletException e){
+            logger.error("Error in newEmployees method. Message: " + e.getMessage());
         }
     }
     private void delete(HttpServletRequest request, HttpServletResponse response){
         try{
             String idemp = request.getParameter("id");
             int id = Integer.parseInt(idemp);
-            modelEmployee.setEmployeeName(request.getParameter("name"));
-            modelEmployee.setEmployeeLastname(request.getParameter("lastname"));
-            modelEmployee.setDepartmentId(Integer.parseInt(request.getParameter("deparment")));
-            modelEmployee.setRolId(Integer.parseInt(request.getParameter("rol")));
-            modelEmployee.setPassword(request.getParameter("password"));
-            modelEmployee.setUsername(request.getParameter("username"));
-            if (empDAO.delete(modelEmployee)) {
+            modelEmployee.setEmployeeId(id);
+            boolean r =empDAO.delete(modelEmployee);
+            if (r) {
                 request.setAttribute("exito","Empleado eliminado");
                 
             }else{
                 request.setAttribute("fracaso","No se ha podido eliminado");
             }
-            request.getRequestDispatcher("/employee.do?op=list").forward(request, response);
-        }catch(IOException | ServletException ex){
-            Logger.getLogger(EmployeeController.class.getName()).log(Level.SEVERE, null, ex);
+            request.getRequestDispatcher("/empleados.do?op=ver").forward(request, response);
+        }catch(IOException | ServletException e){
+            logger.error("Error in deleteEmployees method. Message: " + e.getMessage());
         }
     }
+
+    
+    
 }

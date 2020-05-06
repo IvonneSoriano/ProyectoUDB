bdece057621abe5fe57614c80da83477924113ef
IvonneSoriano <ivonnesoriano25@gmail.com>
2020-05-06 14:23:01 -0600

    validate navbar and index

--- ProyectWeb/src/java/sv/edu/udb/controllers/LoginController.java
+++ ProyectWeb/src/java/sv/edu/udb/controllers/LoginController.java
@@ -121,6 +121,8 @@ public class LoginController extends HttpServlet {
                     session_actual.setAttribute("sessionEmpDeparmentId", e.getDepartmentId());
                     session_actual.setAttribute("username", e.getUsername());
                     session_actual.setAttribute("employee", e);
+                    session_actual.setAttribute("depto", e.getDepartmentId());
+                    session_actual.setAttribute("rol", e.getRolId());
                     session_actual.setAttribute("fullName", e.getFullName());
 
                     // welcome message

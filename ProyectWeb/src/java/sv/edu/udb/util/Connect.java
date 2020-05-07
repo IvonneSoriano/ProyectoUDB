72900dcace0425f1c375bd280eb3093e643ff861
ImerAl <40179626+ImerAl@users.noreply.github.com>
2020-05-06 21:22:11 -0600

    Connect

--- ProyectWeb/src/java/sv/edu/udb/util/Connect.java
+++ ProyectWeb/src/java/sv/edu/udb/util/Connect.java
@@ -10,6 +10,8 @@ import java.io.InputStream;
 import java.sql.*;
 import java.util.Properties;
 import org.apache.log4j.Logger;
+import sv.edu.udb.models.Request;
+import sv.edu.udb.models.Attachment;
 
 /**
  *
@@ -46,10 +48,30 @@ public class Connect {
         }
     }
 
+    public int setPreparedStatementFile(String sql, Request t) {
+        int r = 0;
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setInt(1, t.getIdTypeRequest());
+            ps.setTimestamp(2, t.getRequestDate());
+            ps.setString(3, t.getRequestDescription());
+            ps.setString(4, t.getRequestStatus());
+            ps.setInt(5, t.getProjectId());
+            ps.setInt(6, t.getDepartmentId());
+            ps.setInt(5, t.getProjectId());
+            ps.setBlob(7, t.getFileIS());
+            
+
+          r=  ps.executeUpdate();
+        } catch (Exception e) {
+            logger.error("Failed in prepared statement SQL! Message: " + e.getMessage());
+        }
+        return r;
+    }
     public ResultSet getRs() {
         return rs;
     }
-    
+
     //Metodo que permite fijar la tabla resultado de la pregunta
     //SQL realizada
     public void setRs(String consulta) {
@@ -59,10 +81,25 @@ public class Connect {
             logger.error("ERROR:No encuentro el driver de la BD: " + e2.getMessage() + " Sql: " + consulta);
         }
     }
-    //Metodo que recibe un sql como parametro que sea un update,insert.delete
 
+    //Metodo que recibe un sql como parametro que sea un update,insert.delete
     public int setQuery(String query) throws SQLException {
         return this.s.executeUpdate(query);
+    }
+
+    public void setPreparedStatement(String sql, Attachment t) {
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setInt(1, (int) t.getCommentId());
+            ps.setString(2, t.getAttachmentName());
+            ps.setFloat(3, t.getAttachmentSize());
+            ps.setString(4, t.getContentType());
+            ps.setBlob(5, t.getFileIS());
+
+            ps.executeUpdate();
+        } catch (Exception e) {
+            logger.error("Failed in prepared statement SQL! Message: " + e.getMessage());
+        }
     }
 
     //Metodo que cierra la conexion
@@ -73,3 +110,4 @@ public class Connect {
         return conexion;
      }
 }
+
